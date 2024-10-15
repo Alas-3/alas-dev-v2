bd95d3ab3f6c0a0043b88ef107a7c27d99d7b00f
ac3labador <ac3labador@gmail.com>
2024-10-15 11:01:45 +0000

    getting ready for gsc

--- pages/index.js
+++ pages/index.js
@@ -9,14 +9,17 @@ export default function HomePage() {
         <meta name="description" content="Portfolio of Christopher Ace Labador, showcasing projects and expertise in React & Next.js." />
         <meta property="og:title" content="Christopher Ace Labador | Front-End Engineer" />
         <meta property="og:description" content="Showcasing projects and skills of Ace Labador, a front-end engineer specializing in React & Next.js." />
+        <meta property="og:site_name" content="Christopher Ace Labador | Portfolio"></meta>
         <meta property="og:image" content="https://acelabador.vercel.app/logo.png" />
         <meta property="og:url" content="https://acelabador.vercel.app/" />
         <meta property="og:type" content="website" />
         <meta name="twitter:card" content="summary_large_image" />
-        <meta name="twitter:title" content="Ace Labador | Portfolio" />
+        <meta name="twitter:title" content="Christopher Ace Labador | Portfolio" />
         <meta name="twitter:description" content="Explore the portfolio of Ace Labador, a front-end engineer specializing in React & Next.js." />
-        <meta name="twitter:image" content="https://acelabador.vercel.app.com/logo.png" />
+        <meta name="twitter:image" content="https://acelabador.vercel.app/logo.png" />
         <link rel="canonical" href="https://acelabador.vercel.app/" />
+
+        <meta name="google-site-verification" content="OBwTnzCJhtTRTGjraMYwzcyuh6-Q9XyE-bb0rxaAzOw" />
       </Head>
       <div>
         <Home />
