f3a2bddb6d422cbd1cf58c9cd86d3bc9b4ba6cd8
ac3labador <ac3labador@gmail.com>
2024-10-15 11:18:44 +0000

    added sitemap

---
+++ next-sitemap.config.js
@@ -0,0 +1,12 @@
+// next-sitemap.js
+/** @type {import('next-sitemap').IConfig} */
+const config = {
+    siteUrl: 'https://acelabador.vercel.app', // Change to your site URL
+    generateRobotsTxt: true, // (optional)
+    // Optional, if you have a specific change frequency for certain pages
+    // exclude: ['/excluded-page'], // Exclude any specific pages from the sitemap
+    // Additional configuration options can go here
+  };
+  
+  module.exports = config;
+  
--- next.config.mjs
+++ next.config.mjs
@@ -2,12 +2,14 @@
 const nextConfig = {
   reactStrictMode: true,
   images: {
-    domains: ['cdn1.iconfinder.com',
-    'images.credly.com',
-    'cdn2.iconfinder.com',
-    'cdn4.iconfinder.com',
-    'm.media-amazon.com',
-    'encrypted-tbn0.gstatic.com',], // Add external domains here
+    domains: [
+      'cdn1.iconfinder.com',
+      'images.credly.com',
+      'cdn2.iconfinder.com',
+      'cdn4.iconfinder.com',
+      'm.media-amazon.com',
+      'encrypted-tbn0.gstatic.com',
+    ], // Add external domains here
   },
 };
 
