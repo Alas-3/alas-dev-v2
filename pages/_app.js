99e0d0dcee86a2c5f2e4c78f5e1b15cfdb2c318f
ac3labador <ac3labador@gmail.com>
2024-10-17 14:03:31 +0000

    moved favicons and meta tags to documentjs

--- pages/_app.js
+++ pages/_app.js
@@ -215,48 +215,6 @@ function MyApp({ Component, pageProps }) {
   return (
     <>
       <Head>
-        {/* Global SEO Settings */}
-        <link
-          rel="icon"
-          href="/favicon.ico"
-          sizes="64x64"
-          type="image/x-icon"
-        />
-        <link
-          rel="icon"
-          href="/favicon-16x16.png"
-          sizes="16x16"
-          type="image/png"
-        />
-        <link
-          rel="icon"
-          href="/favicon-32x32.png"
-          sizes="32x32"
-          type="image/png"
-        />
-        <link
-          rel="icon"
-          href="/favicon-48x48.ico"
-          sizes="48x48"
-          type="image/x-icon"
-        />
-        <link
-          rel="apple-touch-icon"
-          sizes="180x180"
-          href="/apple-touch-icon.png"
-        />
-
-        <meta charset="utf-8" />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <meta property="og:type" content="website" />
-        <meta
-          property="og:title"
-          content="Christopher Ace Labador | Portfolio"
-        />
-        <meta property="og:image" content="/images/logo.png" />
-        <meta property="og:locale" content="en_US" />
-        <meta name="twitter:card" content="summary_large_image" />
-
         {/* JSON-LD Script */}
         <script
           type="application/ld+json"
--- pages/_document.js
+++ pages/_document.js
@@ -3,7 +3,50 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        {/* Favicon Links */}
+        <link
+          rel="icon"
+          href="/favicon.ico"
+          sizes="64x64"
+          type="image/x-icon"
+        />
+        <link
+          rel="icon"
+          href="/favicon-16x16.png"
+          sizes="16x16"
+          type="image/png"
+        />
+        <link
+          rel="icon"
+          href="/favicon-32x32.png"
+          sizes="32x32"
+          type="image/png"
+        />
+        <link
+          rel="icon"
+          href="/favicon-48x48.ico"
+          sizes="48x48"
+          type="image/x-icon"
+        />
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/apple-touch-icon.png"
+        />
+
+        {/* Meta Tags */}
+        <meta charSet="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta property="og:type" content="website" />
+        <meta
+          property="og:title"
+          content="Christopher Ace Labador | Portfolio"
+        />
+        <meta property="og:image" content="/images/logo.png" />
+        <meta property="og:locale" content="en_US" />
+        <meta name="twitter:card" content="summary_large_image" />
+      </Head>
       <body className="antialiased">
         <Main />
         <NextScript />
